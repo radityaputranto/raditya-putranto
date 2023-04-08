55f3a610691c0b0ceba98eca55fb71325890a3dc
radityaputranto <radityaputranto97@yahoo.co.id>
2023-04-08 21:26:47 +0700

    init project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,70 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <small>Let's get in touch</small>
+    <h1>Raditya Putranto</h1>
+    <h6>Contact me via:</h6>
+    <div class="grid grid-cols-2 gap-4">
+      <div v-for="item in links" :key="item.id">
+        <a
+          :href="item.url"
+          target="_blank"
+          class="py-2 px-4 font-semibold rounded-lg shadow-md text-white bg-green-500 hover:bg-green-700"
+          ><font-awesome-icon class="mr-2" :icon="['fab', item.icon]" />{{
+            item.name
+          }}</a
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "IndexPage",
+  data() {
+    return {
+      links: [
+        {
+          icon: "linkedin",
+          name: "Linkedin",
+          url: "https://www.linkedin.com/in/radityaputranto/",
+        },
+        {
+          icon: "instagram",
+          name: "Instagram",
+          url: "http://http://instagram.com/raditya_putranto",
+        },
+        {
+          icon: "twitter",
+          name: "twitter",
+          url: "https://twitter.com/radityaputranto",
+        },
+        {
+          icon: "medium",
+          name: "Medium",
+          url: "https://medium.com/@radityaputranto",
+        },
+        {
+          icon: "spotify",
+          name: "Spotify",
+          url: "https://open.spotify.com/user/21ercmyaahviavsid45c74oai?si=sLeETtgCS1GNa3D4V-_Osg&nd=1",
+        },
+        {
+          icon: "soundcloud",
+          name: "Soundcloud",
+          url: "https://soundcloud.com/raditya_putranto",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.btn {
+  @apply py-2 px-4 font-semibold rounded-lg shadow-md;
+}
+.btn-green {
+  @apply text-white bg-green-500 hover:bg-green-700;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import './css/index.css'
 
-createApp(App).mount('#app')
+/* font awesome */
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faLinkedin, faInstagram, faMedium, faTwitter, faSpotify, faSoundcloud } from '@fortawesome/free-brands-svg-icons'
+library.add(faUserSecret, faLinkedin, faInstagram, faMedium, faTwitter, faSpotify, faSoundcloud)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
